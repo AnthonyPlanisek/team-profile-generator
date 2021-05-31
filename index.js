d88ba2966cf54e466206dfedcca6314e5f35f6b3
Plan49 <anthonyplanisek1@gmail.com>
2021-05-30 22:48:34 -0400

    added the first question and test

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+//fs used to write file for my HTML and inquirer for questions
 const fs = require("fs")
+const inquirer = require("inquirer")
+//Pseudo Code
+
+//Generate a basic HTML layout like on gitlab
+//Ask the user for employee information
+//1. Name 2. Job title 3. ID number 4. Email (with link to send email)
+//depending on the job chosen ask a different final question
+//Manager - Office number
+//Engineer - Github (link to github)
+//Intern - School name
+
+//Start of code
+promptName = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "employee",
+            message: "What is the employees name?",
+            validate: (response) => {
+                if (response.length < 1) {
+                return console.log("Field cannot be left blank")
+            }
+             return true
+            },
+        },
+    ])
+    .then((answers) => {
+        console.log(answers)
+    })
+}
+promptName()

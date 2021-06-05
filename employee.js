8c9017b23a8f4df0285edeb3be38350f0773d884
Plan49 <anthonyplanisek1@gmail.com>
2021-06-05 19:54:13 -0400

    added html

--- employee.js
+++ employee.js
@@ -1,43 +1,128 @@
 const inquirer = require("inquirer")
 
 class Employee {
-        constructor(name) {
-        this.name = name
-    }
-
-    employeeName() {
+ 
+    confirmE = () => {
         inquirer
+        .prompt([
+            {
+                type: "confirm",
+                name: "add",
+                default: true
+            },
+        ])
+    
+        .then((answers1) => {
+            if (answers1.add === true) {
+                console.log("answer", answers1)
+                employeeNameType()
+            } else {
+                console.log("You are all done")
+    
+            }
+            
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+              // Prompt couldn't be rendered in the current environment
+            } else {
+              // Something else went wrong
+            }
+          });
+    }
+    
+       employeeNameType = () => {
+    
+       
+            inquirer
+                .prompt([
+                    {
+                        
+                        type: "input",
+                        name: "employee",
+                        message: "What is the employees name?",
+                        validate: (response) => {
+                            if (response.length < 1) {
+                            return console.log("Field cannot be left blank")
+                        }
+                            return true
+                        },
+                        
+                    },
+                    {
+                        type: "list",
+                        name: "jobTitle",
+                        message: "What is the employees job type",
+                        choices: ["Manager", "Engineer", "Intern"]
+                    }
+                ])
+                .then((answers2) => {
+                    console.log("answers", answers2)
+                    
+                    if (answers2.jobTitle === "Manager") {
+                        console.log("hello")
+                        manager();
+                    }if (answers2.jobTitle === "Engineer") {
+                        engineer();
+                    }if (answers2.jobTitle === "Intern") {
+                        intern();
+                    }
+                    
+                })
+                .catch((error) => {
+                    if (error.isTtyError) {
+                  []    // Prompt couldn't be rendered in the current environment
+                    } else {
+                      // Something else went wrong
+                    }
+                  });
+                }
+        
+    manager = () => {
+        inquirer 
             .prompt([
                 {
-                    
                     type: "input",
-                    name: "employee",
-                    message: "What is the employees name?",
-                    validate: (response) => {
-                        if (response.length < 1) {
-                        return console.log("Field cannot be left blank")
-                    }
-                        return true
-                    },
-                    
-                },
-                {
-                    type: "list",
-                    name: "jobTitle",
-                    message: "What is the employees job type",
-                    choices: ["Manager", "Engineer", "Intern"]
+                    name: "officeNum",
+                    message: "What is this managers office number?"
                 }
             ])
-            .then((answers) => {
-                console.log("answer", answers)
+            .then((answers3) =>{
+                console.log("answers", answers3)
+                confirmE()
             })
-            .catch((error) => {
-                if (error.isTtyError) {
-                  // Prompt couldn't be rendered in the current environment
-                } else {
-                  // Something else went wrong
+    }
+    
+    
+    engineer = () => {
+        inquirer 
+            .prompt([
+                {
+                    type: "input",
+                    name: "github",
+                    message: "What is this engineers github?"
                 }
-              });
+            ])
+            .then((answers4) =>{
+                console.log("answers", answers4)
+                confirmE()
+            })
+    }
+    
+    
+    intern = () => {
+        inquirer 
+            .prompt([
+                {
+                    type: "input",
+                    name: "school",
+                    message: "What is this interns school?"
+                }
+            ])
+            .then((answers6) =>{
+                console.log("answers", answers6)
+                confirmE()
+            })
     }
     
 }
--- index.js
+++ index.js
@@ -2,50 +2,19 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
 const Employee = require("./employee")
-//Pseudo Code
+const employee = new Employee()
+employee.employeeNameType()
 
-//Generate a basic HTML layout like on gitlab
-//Ask the user for employee information
-//1. Name 2. Job title 3. ID number 4. Email (with link to send email)
-//depending on the job chosen ask a different final question
-//Manager - Office number
-//Engineer - Github (link to github)
-//Intern - School name
 
-//Start of code (code needs to run x amount of times per employee)
-let confirmPromise = new Promise(function() {
+/*
+content = 
+`
 
-    inquirer
-        .prompt([
-            {
-                type: "confirm",
-                name: "add",
-                default: true
-            },
-        ])
-    
-        .then((answers) => {
-            if (answers.add === true) {
-                console.log("answer", answers)
-            } else {
-                console.log("Your all done")
-            }
-            
-        })
-        .catch((error) => {
-            if (error.isTtyError) {
-              // Prompt couldn't be rendered in the current environment
-            } else {
-              // Something else went wrong
-            }
-          });
-        })
+`
 
-let newPromise = new Promise(function() {
-const createEmployee = new Employee("john")
-createEmployee.employeeName()
-})
 
-newPromise.then(
-    confirmPromise.then()
-)
+
+fs.appendFile("team.html", content, function (err) {
+    if (err) throw err;
+  console.log('Saved!');
+})'*/
